132f7e88d25af52a344ab2f364ba8ca98ef94343
dukeofgaming <dukeofgaming@gmail.com>
2021-10-08 15:29:56 -0500

    Improving layout with Grid and Paper, and some very basic validation and feedback to user

--- src/src/main/react/TemperatureConversionGradingApp.jsx
+++ src/src/main/react/TemperatureConversionGradingApp.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import ReactDOM from 'react-dom';
 import TextField from '@mui/material/TextField';
+import Box from '@mui/material/box';
+import Grid from '@mui/material/grid';
+import Paper from '@mui/material/Paper';
 
 class TemperatureConversionGradingApp extends React.Component {
     constructor(props){
@@ -17,17 +20,19 @@ class TemperatureConversionGradingApp extends React.Component {
         var target_unit         = element.querySelector("input[name=target_unit]").value;
         var student_response    = element.querySelector("input[name=student_response]").value;
 
+        var output_element = element.querySelector("#output");
+
         if(
             input_value         === "" ||
             input_unit          === "" ||
             target_unit         === "" ||
             student_response    === ""
         ){
+            output_element.innerHTML = '<i>Incomplete input</i>';
             console.log("Incomplete input, cancelling request to server");
             return;
         }
 
-        var output_element = element.querySelector("div[id=output]");
 
         var rounded_student_response    = Math.round(student_response*10)/10;
 
@@ -50,13 +55,14 @@ class TemperatureConversionGradingApp extends React.Component {
                 console.log("Authoritative answer: " + authoritative_answer + " (" + typeof authoritative_answer +")");
                 console.log("Student (rounded): " + rounded_student_response + " (" + typeof rounded_student_response +")");
 
-
-                output_element.innerHTML = (authoritative_answer === "invalid")?(
-                    "invalid"
-                ):((parseFloat(authoritative_answer) === rounded_student_response))?(
-                    "correct"
-                ):(
-                    "incorrect"
+                output_element.innerHTML = "<b>Output:</b>&nbsp;" + (
+                    (authoritative_answer === "invalid")?(
+                        "invalid"
+                    ):((parseFloat(authoritative_answer) === rounded_student_response))?(
+                        "correct"
+                    ):(
+                        "incorrect"
+                    )
                 );
 
             }
@@ -65,33 +71,68 @@ class TemperatureConversionGradingApp extends React.Component {
 
     render() {
         return (
-            <div>
-                <TextField
-                    variant="outlined"
-                    label="Input Numerical Value"
-                    name="input_value"
-                    onBlur={this.grade}
-                />
-                <TextField
-                    variant="outlined"
-                    label="Input Unit of Measure"
-                    name="input_unit"
-                    onBlur={this.grade}
-                />
-                <TextField
-                    variant="outlined"
-                    label="Target Unit of Measure"
-                    name="target_unit"
-                    onBlur={this.grade}
-                />
-                <TextField
-                    variant="outlined"
-                    label="Student Response"
-                    name="student_response"
-                    onBlur={this.grade}
-                />
-                <div id="output" ></div>
-            </div>
+            <Box sx={{
+                width: '450px'
+            }}>
+                <Paper
+                    elevation={2}
+                    sx={{
+                        padding: '15px'
+                }}>
+                    <Grid container spacing={2} columns={2}>
+                        <Grid item xs={2} sx={{
+                            'font-weight': 'bolder',
+                            'text-align': 'center'
+                        }}>
+                            Science Unit: Unit Converter
+                        </Grid>
+                        <Grid item xs={1}>
+                            <TextField
+                                variant="outlined"
+                                label="Input Numerical Value"
+                                name="input_value"
+                                onBlur={this.grade}
+                            />
+                        </Grid>
+                        <Grid item xs={1}>
+                            <TextField
+                                variant="outlined"
+                                label="Input Unit of Measure"
+                                name="input_unit"
+                                onBlur={this.grade}
+                            />
+                        </Grid>
+                        <Grid item xs={1}>
+                            <TextField
+                                variant="outlined"
+                                label="Student Response"
+                                name="student_response"
+                                onBlur={this.grade}
+                            />
+                        </Grid>
+                        <Grid item xs={1}>
+                            <TextField
+                                variant="outlined"
+                                label="Target Unit of Measure"
+                                name="target_unit"
+                                onBlur={this.grade}
+                            />
+                        </Grid>
+                        <Grid item xs={2}>
+                            <Paper
+                                id="output"
+                                elevation={4}
+                                sx={{
+                                    padding: '15px',
+                                    'text-transform': 'capitalize',
+                                    'text-align': 'center'
+                            }}>
+                                <i>Please fill the form above</i>
+                            </Paper>
+                        </Grid>
+                    </Grid>
+                </Paper>
+            </Box>
         );
     }
 }
