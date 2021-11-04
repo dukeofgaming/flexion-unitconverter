02cffa05f77710d9f2defcfa712757615e12e85a
dukeofgaming <dukeofgaming@gmail.com>
2021-11-04 10:59:39 -0500

    - Changing version.txt to environment variable so that it can be read more easily from the application itself - Adding hostname + version display at the bottom of the UI

--- src/src/main/java/com/scienceunit/unitconverter/UnitconverterApplication.java
+++ src/src/main/java/com/scienceunit/unitconverter/UnitconverterApplication.java
@@ -3,8 +3,12 @@ package com.scienceunit.unitconverter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.io.IOException;
+import java.util.Scanner;
 
 @SpringBootApplication
 @Controller
@@ -16,7 +20,16 @@ public class UnitconverterApplication {
 	}
 
 	@GetMapping(value = {"","/"})
-	public String home() {
+	public String home(Model model) throws IOException{
+
+		model.addAttribute(
+			"hostname",
+			new Scanner(Runtime.getRuntime().exec("hostname").getInputStream()).useDelimiter("\\A").next()
+		).addAttribute(
+			"version",
+			System.getenv("APP_VERSION")
+		);
+
 		return "home";
 	}
 }
